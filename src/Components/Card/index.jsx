987a02068806825abe5f485c3a39fd0f9fe4d7a5
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2023-09-09 17:39:04 -0500

    get all products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,24 @@
-const Card = () => {
+import PropTypes from 'prop-types'
+
+const Card = ({ price, title, description, category, images }) => {
+  Card.propTypes = {
+    price: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.object,
+    images: PropTypes.array
+  }
+
   return (
     <div className="bg-white cursor-pointer w-56 h-60">
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 py-3 px-0.5">Electronics</span>
-        <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headphones" />
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 py-1 px-1">{category?.name}</span>
+        <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={description} />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light">{title}</span>
+        <span className="text-lg font-medium">{`$ ${price}`}</span>
       </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react"
 import Card from "../../Components/Card"
 
 const Home = () => {
+
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    fetch( 'https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
   return (
     <>
-      home
-      <Card />
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          products?.map((product) => {
+            console.log(product)
+            return <Card key={product.id} { ...product }/>
+          })
+        }
+      </div>
     </>
   )
 }
